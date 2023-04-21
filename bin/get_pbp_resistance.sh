431f2f6ef7d85a67fe47f659f5db56cc5f42f87b
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-04-21 15:21:11 +0000

    Change output of AMR

--- bin/get_pbp_resistance.sh
+++ bin/get_pbp_resistance.sh
@@ -1,7 +1,6 @@
 # Extract the results from the output file of the PBP AMR predictor
 
 # For all, replace null or space-only string with empty string
-# For resistances, S, I and R in the file are output as SENSITIVE, INTERMEDIATE and RESISTANT respectively
 
 function GET_VALUE {
     echo $( < $JSON_FILE jq -r --arg target "$1" '.[$target]' \
@@ -10,7 +9,7 @@ function GET_VALUE {
 
 function GET_RES {
     echo $( < $JSON_FILE jq -r --arg target "$1" '.[$target]' \
-        | sed 's/^S$/SENSITIVE/g;s/^I$/INTERMEDIATE/g;s/^R$/RESISTANT/g;s/^null$//g;s/^\s+$//g' )
+        | sed 's/^null$//g;s/^\s+$//g' )
 }
 
 pbp1a=$(GET_VALUE "pbp1a")
