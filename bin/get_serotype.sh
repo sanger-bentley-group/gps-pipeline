5ccce23208e6dd67da42c17af37be37d7d5880c0
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-07-04 15:38:41 +0000

    Protect pipeline against SeroBA failure
    Former-commit-id: 424d5fe075624dda2bd404664a8645e8c384b30b

--- bin/get_serotype.sh
+++ bin/get_serotype.sh
@@ -1,5 +1,6 @@
 # Run SeroBA to serotype samples
-
-seroba runSerotyping "$SEROBA_DIR"/"$DATABASE" "$READ1" "$READ2" "$SAMPLE_ID"
-
-SEROTYPE=$(awk -F'\t' '{ print $2 }' ${SAMPLE_ID}/pred.tsv)
+{
+    seroba runSerotyping "$SEROBA_DIR"/"$DATABASE" "$READ1" "$READ2" "$SAMPLE_ID" && SEROTYPE=$(awk -F'\t' '{ print $2 }' ${SAMPLE_ID}/pred.tsv)
+} || {
+    SEROTYPE="SEROBA_FAILURE"
+}
