ae3cce669b98644ec3ca8e96c9586bf648ba0b39
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-07-27 14:57:36 +0000

    Improve shell scripts style
    Former-commit-id: 60614c8ef117a5641b4fbbf92343e2721e6d27db

--- bin/get_docker_compose.sh
+++ bin/get_docker_compose.sh
@@ -2,13 +2,13 @@
 
 COUNT=0
 
-echo "services:" >> $COMPOSE
+echo "services:" >> "$COMPOSE"
 
-grep -E "container\s?=" $NEXTFLOW_CONFIG \
+grep -E "container\s?=" "$NEXTFLOW_CONFIG" \
     | sort -u \
     | sed -r "s/\s+container\s?=\s?'(.+)'/\1/" \
     |   while read -r IMAGE ; do
             COUNT=$((COUNT+1))
-            echo "  SERVICE${COUNT}:" >> $COMPOSE
-            echo "      image: $IMAGE" >> $COMPOSE
+            echo "  SERVICE${COUNT}:" >> "$COMPOSE"
+            echo "      image: $IMAGE" >> "$COMPOSE"
         done
