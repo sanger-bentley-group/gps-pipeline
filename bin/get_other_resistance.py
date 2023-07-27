e0d2010b02cb46362e05f815e3e1e3552a5affba
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-07-27 17:10:04 +0000

    Refactor to improve maintainability & readability
    Former-commit-id: b813ad0a8aba3f2e252b5d00d659e96e64d024cc

--- bin/get_other_resistance.py
+++ bin/get_other_resistance.py
@@ -8,72 +8,97 @@ from collections import defaultdict
 import pandas as pd
 import csv
 
-report_path = sys.argv[1]
-debug_report_path = sys.argv[2]
-metadata_path = sys.argv[3]
-output_file = sys.argv[4]
 
-with open(report_path) as report, open(debug_report_path) as debug_report, open(metadata_path) as metadata:
+# Check argv and save to global variables
+if len(sys.argv) != 5:
+    sys.exit('Usage: get_other_resistance.py REPORT_PATH DEBUG_REPORT_PATH METADATA_PATH OUTPUT_FILE')
+
+REPORT_PATH = sys.argv[1]
+DEBUG_REPORT_PATH = sys.argv[2]
+METADATA_PATH = sys.argv[3]
+OUTPUT_FILE = sys.argv[4]
+
+
+def main():
+    targets_dict, hits_dict = prepare_dicts()
+    find_hits(targets_dict, hits_dict)
+    output = get_output(hits_dict)
+
+    # Save output to OUTPUT_FILE in csv format
+    pd.DataFrame([output]).to_csv(OUTPUT_FILE, index=False, quoting=csv.QUOTE_ALL)
+
+
+def prepare_dicts():
     # For saving (reference, gene, var_only) combinations as key and their information ({var_change: target}) as value found in metadata
-    gene_dict = defaultdict(dict)
+    # Used to search whether there is a hit in the ARIBA result
+    targets_dict = defaultdict(dict)
 
-    # For saving targets found in metadata as key and their determinants (add to a set) as value
-    target_dict = {}
+    # For saving targets found in metadata as key and their determinants (i.e. hits) found in ARIBA result as values in set
+    hits_dict = {}
 
-    # Skip the header in metadata
-    next(metadata)
-    # Go through lines in metadata and save findings to gene_dict and target_dict
-    for line in (line.strip() for line in metadata):
-        # Extract useful fields
-        fields = [str(field) for field in line.split("\t")] 
-        ref_name, gene, var_only, var_change, _, target = fields
+    with open(METADATA_PATH) as metadata:
+        # Skip the header in metadata
+        next(metadata)
 
-        # Populating gene_dict
-        gene_dict[(ref_name, gene, var_only)].update({var_change: target})
-        # Populating target_dict
-        target_dict.update({target: set()})
+        # Go through lines in metadata and save findings to targets_dict and hits_dict
+        for line in (line.strip() for line in metadata):
+            # Extract useful fields
+            fields = [str(field) for field in line.split("\t")] 
+            ref_name, gene, var_only, var_change, _, target = fields
 
-    # Skip the header in report and debug report
-    next(report)
-    next(debug_report)
-    # Go through lines in both report and debug report to detect targets
-    for line in (line.strip() for line in chain(report, debug_report)):
-        # Extract useful fields
-        fields = [str(field) for field in line.split("\t")]
-        ref_name, gene, var_only, ref_len, ref_base_assembled, known_var_change, has_known_var, ref_ctg_effect, ref_start, ref_end = fields[1], fields[2], fields[3], fields[7], fields[8], fields[16], fields[17], fields[19], fields[20], fields[21]
+            # Populating targets_dict
+            targets_dict[(ref_name, gene, var_only)].update({var_change: target})
+            # Populating hits_dict
+            hits_dict.update({target: set()})
+    
+    return targets_dict, hits_dict
 
-        # If coverage (ref_base_assembled / ref_len) < 0.9 or either variable contains non-numeric value, skip the line
-        if not ref_base_assembled.isdigit() or not ref_len.isdigit() or int(ref_base_assembled)/int(ref_len) < 0.9:
-            continue
-        
-        # If the known_var_change (. for genes, specific change for variants) is not found in the metadata of the (ref_name, gene, var_only) combination, skip the line
-        gene_dict_key = (ref_name, gene, var_only)
-        try:
-            target = gene_dict[gene_dict_key][known_var_change]
-        except KeyError: 
-            continue
 
-        # Logic for gene detection. Found means hit.
-        if var_only == "0":
-            target_dict[target].add(f'{ref_name}')
-        
-        # Logic for variant detection, further criteria required
-        if var_only == "1":
-            # folP-specific criteria: ref_ctg_effect (effect of change between reference and contig) is one of the keywords and the change occurs within nt 168-201
-            if ref_name.lower().startswith("folp") and ref_ctg_effect.lower() in ('fshift', 'trunc', 'indel', 'ins', 'multiple') and (168 <= int(ref_start) <= 201 or 168 <= int(ref_end) <= 201):
-                pos = ref_start if ref_start == ref_end else f'{ref_start}-{ref_end}'
-                target_dict[target].add(f'{ref_name} {ref_ctg_effect} at {pos}')
-            # Common criteria: the assembly has that variant
-            elif has_known_var == "1":
-                target_dict[target].add(f'{ref_name} Variant {known_var_change}')
+def find_hits(targets_dict, hits_dict):
+    with open(REPORT_PATH) as report, open(DEBUG_REPORT_PATH) as debug_report:
+        # Skip the header in report and debug report
+        next(report)
+        next(debug_report)
 
+        # Go through lines in both report and debug report to detect targets
+        for line in (line.strip() for line in chain(report, debug_report)):
+            # Extract useful fields
+            fields = [str(field) for field in line.split("\t")]
+            ref_name, gene, var_only, ref_len, ref_base_assembled, known_var_change, has_known_var, ref_ctg_effect, ref_start, ref_end = fields[1], fields[2], fields[3], fields[7], fields[8], fields[16], fields[17], fields[19], fields[20], fields[21]
+
+            # If coverage (ref_base_assembled / ref_len) < 0.9 or either variable contains non-numeric value, skip the line
+            if not ref_base_assembled.isdigit() or not ref_len.isdigit() or int(ref_base_assembled)/int(ref_len) < 0.9:
+                continue
+            
+            # If the known_var_change (. for genes, specific change for variants) is not found in the metadata of the (ref_name, gene, var_only) combination, skip the line
+            try:
+                target = targets_dict[(ref_name, gene, var_only)][known_var_change]
+            except KeyError: 
+                continue
+
+            # Logic for gene detection. Found means hit.
+            if var_only == "0":
+                hits_dict[target].add(f'{ref_name}')
+            
+            # Logic for variant detection, further criteria required
+            if var_only == "1":
+                # folP-specific criteria: ref_ctg_effect (effect of change between reference and contig) is one of the keywords and the change occurs within nt 168-201
+                if ref_name.lower().startswith("folp") and ref_ctg_effect.lower() in ('fshift', 'trunc', 'indel', 'ins', 'multiple') and (168 <= int(ref_start) <= 201 or 168 <= int(ref_end) <= 201):
+                    pos = ref_start if ref_start == ref_end else f'{ref_start}-{ref_end}'
+                    hits_dict[target].add(f'{ref_name} {ref_ctg_effect} at {pos}')
+                # Common criteria: the assembly has that variant
+                elif has_known_var == "1":
+                    hits_dict[target].add(f'{ref_name} Variant {known_var_change}')
+
+
+def get_output(hits_dict):
     # For saving final output, where information is saved per-target
     output = {}
 
-    # Go through targets in metadata
-    for target in target_dict:
+    # Go through targets in hits_dict
+    for target in hits_dict:
         # If the target has no hit, set output as S or NEG (only for PILI-1/2), and determinant as _
-        if len(target_dict[target]) == 0:
+        if len(hits_dict[target]) == 0:
             if target.lower().startswith('pili'):
                 output[target] = 'NEG'
             else:
@@ -87,10 +112,15 @@ with open(report_path) as report, open(debug_report_path) as debug_report, open(
             else:
                 output[f'{target}_Res'] = 'R'
 
-            output[f'{target}_Determinant'] = '; '.join(target_dict[target])
+            output[f'{target}_Determinant'] = '; '.join(sorted(hits_dict[target]))
 
-    # Special cases to add to output
+    add_output_special_cases(output, hits_dict)
 
+    return output
+
+
+# Special cases to add to output
+def add_output_special_cases(output, hits_dict):
     # If TET exists and DOX does not: add DOX to output; directly copy output and determinant
     if 'TET_Res' in output and 'DOX_Res' not in output:
         output['DOX_Res'] = output['TET_Res']
@@ -107,15 +137,15 @@ with open(report_path) as report, open(debug_report_path) as debug_report, open(
     if 'TMP_Res' in output and 'SMX_Res' in output and 'COT_Res' not in output:
         if output['TMP_Res'] == 'R' and output['SMX_Res'] == 'R':
             output['COT_Res'] = 'R'
-            output['COT_Determinant'] = '; '.join(target_dict['TMP'].union(target_dict['SMX']))
+            output['COT_Determinant'] = '; '.join(sorted(hits_dict['TMP'].union(hits_dict['SMX'])))
         elif (output['TMP_Res'] == 'R') ^ (output['SMX_Res'] == 'R'):
             output['COT_Res'] = 'I'
-            output['COT_Determinant'] = '; '.join(target_dict['TMP'].union(target_dict['SMX']))
+            output['COT_Determinant'] = '; '.join(sorted(hits_dict['TMP'].union(hits_dict['SMX'])))
         elif output['TMP_Res'] == 'S' and output['SMX_Res'] == 'S':
             output['COT_Res'] = 'S'
             output['COT_Determinant'] = '_'
 
-    # If ERY_CLI exists, add ERY and CLI to output.
+    # If ERY_CLI exists: add ERY and CLI to output.
     # If ERY_CLI is R, ERY and CLI are R, and add ERY_CLI determinant to their determinants
     # If ERY_CLI is S, ERY and CLI are S if they do not already exist, otherwise leave them unchanged
     if 'ERY_CLI_Res' in output:
@@ -126,8 +156,9 @@ with open(report_path) as report, open(debug_report_path) as debug_report, open(
             output['ERY_Res'] = output['ERY_Res'] if 'ERY_Res' in output else 'S'
             output['CLI_Res'] = output['CLI_Res'] if 'CLI_Res' in output else 'S'
         
-        output['ERY_Determinant'] = '; '.join(target_dict['ERY_CLI'].union(target_dict['ERY'])) if 'ERY' in target_dict and len(target_dict['ERY']) != 0 else output['ERY_CLI_Determinant']
-        output['CLI_Determinant'] = '; '.join(target_dict['ERY_CLI'].union(target_dict['CLI'])) if 'CLI' in target_dict and len(target_dict['CLI']) != 0 else output['ERY_CLI_Determinant']
+        output['ERY_Determinant'] = '; '.join(sorted(hits_dict['ERY_CLI'].union(hits_dict['ERY']))) if 'ERY' in hits_dict and len(hits_dict['ERY']) != 0 else output['ERY_CLI_Determinant']
+        output['CLI_Determinant'] = '; '.join(sorted(hits_dict['ERY_CLI'].union(hits_dict['CLI']))) if 'CLI' in hits_dict and len(hits_dict['CLI']) != 0 else output['ERY_CLI_Determinant']
 
-    # Save output dict as csv
-    pd.DataFrame([output]).to_csv(output_file, index=False, quoting=csv.QUOTE_ALL)
+
+if __name__ == "__main__":
+    main()
