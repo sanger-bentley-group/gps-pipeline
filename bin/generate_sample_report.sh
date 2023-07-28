63ad10beee0e35b3578e30c985a4e9fe732a4637
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2023-07-28 11:14:12 +0000

    Improve shell scripts style

--- bin/generate_sample_report.sh
+++ bin/generate_sample_report.sh
@@ -1,3 +1,5 @@
+# Combine all csv reports into a single csv, then add Sample_ID as the first field
+
 paste -d , *.csv \
 | sed '1 s/^/\"Sample_ID\",/' \
 | sed "2 s/^/\"${SAMPLE_ID}\",/" > "$SAMPLE_REPORT"
--- bin/get_poppunk_ext_clusters.sh
+++ bin/get_poppunk_ext_clusters.sh
@@ -4,20 +4,19 @@
 # If not: remove all csv files, and download to database directory, also save metadata to JSON
 
 EXT_CLUSTERS_CSV=$(basename "$EXT_CLUSTERS_REMOTE")
-EXT_CLUSTERS_NAME=$(basename "$EXT_CLUSTERS_REMOTE" .csv)
 
-if  [ ! -f ${EXT_CLUSTERS_LOCAL}/${JSON_FILE} ] || \
-    [ ! "$EXT_CLUSTERS_REMOTE" == "$(jq -r .url ${EXT_CLUSTERS_LOCAL}/${JSON_FILE})"  ] || \
-    [ ! -f ${EXT_CLUSTERS_LOCAL}/${EXT_CLUSTERS_CSV} ]; then
+if  [ ! -f "${EXT_CLUSTERS_LOCAL}/${JSON_FILE}" ] || \
+    [ ! "$EXT_CLUSTERS_REMOTE" == "$(jq -r .url "${EXT_CLUSTERS_LOCAL}/${JSON_FILE}")"  ] || \
+    [ ! -f "${EXT_CLUSTERS_LOCAL}/${EXT_CLUSTERS_CSV}" ]; then
 
-    rm -f ${EXT_CLUSTERS_LOCAL}/*.csv
-    rm -f ${EXT_CLUSTERS_LOCAL}/${JSON_FILE}
+    rm -f "${EXT_CLUSTERS_LOCAL}"/*.csv
+    rm -f "${EXT_CLUSTERS_LOCAL}/${JSON_FILE}"
 
-    wget $EXT_CLUSTERS_REMOTE -O ${EXT_CLUSTERS_LOCAL}/${EXT_CLUSTERS_CSV}
+    wget "$EXT_CLUSTERS_REMOTE" -O "${EXT_CLUSTERS_LOCAL}/${EXT_CLUSTERS_CSV}"
 
     jq -n \
         --arg url "$EXT_CLUSTERS_REMOTE" \
         --arg save_time "$(date +"%Y-%m-%d %H:%M:%S %Z")" \
-        '{"url" : $url, "save_time": $save_time}' > ${EXT_CLUSTERS_LOCAL}/${JSON_FILE}
+        '{"url" : $url, "save_time": $save_time}' > "${EXT_CLUSTERS_LOCAL}/${JSON_FILE}"
 
 fi
